399e1fd787ea96ee4d64c45bad33b96eed45af4d
Danci Romina Alina <65411784+RominaDanci@users.noreply.github.com>
2022-07-04 10:18:20 +0200

    Gitignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "VIRTUAL_ENV=C:\Users\giorg\OneDrive\Desktop\Tirocinio\venv"
+set "VIRTUAL_ENV=C:\Users\romi0\Documents\GitHub\Tirocinio\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
