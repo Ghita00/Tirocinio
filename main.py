1ce0ef1a17922e660539d4f1a689181c5e4d5df9
Giorgio Basile <giorgiobasile631@gmail.com>
2022-07-05 09:34:29 +0200

    adattamento del template per il sito a Jinja

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, redirect
-from GenDB import db
+#from GenDB import db
 
 app = Flask(__name__)
 
-db.create_all()
+#db.create_all()
 
 
 @app.route('/')
 def hello_world():
-    return render_template("sito/about.html")
+    return render_template("sito/blog.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
