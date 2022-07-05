d545ff3ea87c520bd17b357f14e82aeebc915f1b
Giorgio Basile <giorgiobasile631@gmail.com>
2022-07-05 18:53:56 +0200

    conversione tema sito, mancano delle parti in shop

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from GenDB import db
 
 app = Flask(__name__)
 
-db.create_all()
+#db.create_all()
 
 
 @app.route('/')
 def hello_world():
-    return render_template("sito/blog-details.html")
+    return render_template("sito/shop.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
