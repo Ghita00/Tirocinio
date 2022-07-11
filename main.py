3efc918c7f8bbd517cacb1f5bc8c8edd851d8eae
Giorgio Basile <giorgiobasile631@gmail.com>
2022-07-11 15:06:58 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, redirect
-from GenDB import *
+from GenDB import db
 from flask_fontawesome import FontAwesome
+
+#blueprint
+from Profile import profile
+from Ecommerce import ecommerce
+from Blog import blog
 
 app = Flask(__name__)
 fa = FontAwesome(app) #serve per i font
+db.create_all() #serve per il db
 
-db.create_all()
+#registrazione blueprint
+app.register_blueprint(profile, url_prefix = "")
+app.register_blueprint(ecommerce, url_prefix = "")
+app.register_blueprint(blog, url_prefix = "")
 
 @app.route('/')
 def hello_world():
-    return render_template("gestionale/table-row-select.html")
+    return render_template("sito/about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
