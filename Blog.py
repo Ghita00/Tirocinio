f48d3a512a77460a319bb7347e213f5baa21fdc0
Giorgio Basile <giorgiobasile631@gmail.com>
2022-07-22 11:01:52 +0200

    fix ux design sparsi

--- Blog.py
+++ Blog.py
@@ -6,6 +6,10 @@ blog = Blueprint('blog', __name__)
 def blogRoute():
     return render_template("sito/blog.html")
 
+@blog.route('/blog-details')
+def blogDetailsRoute():
+    return render_template("sito/blog-details.html")
+
 #gestionale
 @blog.route("/gestionale/blog")
 def Gblog():
