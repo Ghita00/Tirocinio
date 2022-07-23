116d9aee3daa61645a63ae43488e560b2ade3c3d
Giorgio Basile <giorgiobasile631@gmail.com>
2022-07-23 15:42:36 +0200

    sort dei risultati in vendita e migliorato la ux delle varie pagine

--- Ecommerce.py
+++ Ecommerce.py
@@ -1,16 +1,16 @@
 from flask import Blueprint, render_template, url_for
 from flask_login import login_required, current_user
 from werkzeug.utils import redirect
-
 from GenDB import *
-
 
 ecommerce = Blueprint('ecommerce', __name__)
 
-@ecommerce.route('/shop')
-def shop():
-    #TODO AGGIUNGERE UN PARAMETRO ALLA ROUTE CHE INDICA IL SORT DA FARE
-    Prodotti = Semilavorati.query.all()
+@ecommerce.route('/shop/<id>')
+def shop(id):
+    if id == 1:
+        Prodotti = Semilavorati.query.order_by(Semilavorati.Nome).all()
+    else:
+        Prodotti = Semilavorati.query.order_by(Semilavorati.PrezzoUnitario).all()
     return render_template("sito/shop.html",Prod = list(Prodotti), lenProd = len(list(Prodotti)))
 
 @ecommerce.route('/shop-details')
@@ -28,6 +28,7 @@ def checkout():
 @ecommerce.route('/wishlist')
 @login_required
 def wishlist():
+    #TODO VEDERE SE IL FRA E LOGGATTO
     list_wishlist = Semilavorati.query.join(WishList).filter(WishList.Mail_Cliente == current_user.Mail).filter(WishList.Id_Semilavorato == Semilavorati.Id).all()
     return render_template("sito/wishlist.html", product = list(list_wishlist), len_product = len(list(list_wishlist)))
 
