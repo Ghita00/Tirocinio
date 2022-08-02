2f63b906cc4c7699d574e59de8383e1d92ae8953
Giorgio Basile <giorgiobasile631@gmail.com>
2022-08-02 11:21:49 +0200

    aggiunti file html e iniziato a customizzare gestionale, aggiunte nuove route

--- Ricettario.py
+++ Ricettario.py
@@ -6,3 +6,12 @@ ricettario = Blueprint('ricettario', __name__)
 @ricettario.route('/ricettarioGestionale')
 def ricettarioGestionale():
     return render_template("gestionale/ricettario.html")
+
+@ricettario.route('/ricetta/<id>')
+def ricetta(id):
+    return render_template("gestionale/ricetteSingle.html")
+
+@ricettario.route('/addRicetta')
+def aggiungiRicetta():
+    return render_template("gestionale/formRicette.html")
+
