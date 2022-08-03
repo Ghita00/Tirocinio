a8949ff0390c0b7de716bcfd9cb7c116e26b8cf1
Giorgio Basile <giorgiobasile631@gmail.com>
2022-08-03 19:23:43 +0200

    impostato Ordini anche lato backend e Produzione giornaliera solo front end

--- Ordini.py
+++ Ordini.py
@@ -1,12 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import current_user
 
 ordini = Blueprint('ordini', __name__)
 
-@ordini.route('/ordiniGestionale')
+@ordini.route('/ordiniGestionale', methods=['GET', 'POST'])
 def ordiniGestionale():
-    return render_template("gestionale/ordini.html")
+    list_ricevuti = []  # TODO QUERY
+    list_emessi = []  # TODO QUERY
+    if request.method == "POST":
+        ricevuti = 0
+        emessi = 0
+        if request.form["nascosto"] == '1':
+            ricevuti = request.form["ricevuti"]
+        else:
+            emessi = request.form["emessi"]
+        #if(ricevuti == 1):
+            # slider ricevuti avanti di 1
+        #if(ricevuti == -1):
+            # slider ricevuti indietro di 1
+        # if(emessi == 1):
+            # slider emessi avanti di 1
+        # if(emessi == -1):
+            # slider emessi indietro di 1
+        return render_template("gestionale/ordini.html")
+    else:
+        #passare sempre i primi 10 elementi delle liste
+        return render_template("gestionale/ordini.html")
 
 @ordini.route('/newOrdine')
 def newOrdine():
-    return render_template("gestionale/nuovoOrdini.html")
+    return render_template("gestionale/nuovoOrdine.html")
